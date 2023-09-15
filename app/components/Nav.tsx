1af3680230039e8f25fedbe0cae0cda45f7d3909
Naol Melesse <naolmelesse91@gmail.com>
2023-09-15 22:54:05 +0300

    nav modified

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -9,7 +9,7 @@ export default function Nav(){
         <nav className=' py-10 flex justify-between w-4/5 mx-[auto] bg-white px-10 md:px-20 lg:px-30'>
 
         <h1 className='font-burtons text-xl' >{"</NM>"}</h1>
-        <div className='invisible lg:visible flex items-center p-2  bg-[#F5F5F5] z-10 fixed left-[35%] rounded-full' >
+        <div className='invisible lg:visible flex items-center p-2  bg-[#F5F5F5] z-10 fixed left-1/3 rounded-full' >
           {/* <li><BsFillMoonStarsFill onClick={()=> setDarkMode(!darkMode) } className=' cursor-pointer'/></li> */}
           <div className="flex justify-center gap-2 mx-4 items-center"><BiHomeAlt2 className="text-xl"/> <Link className='hover:underline hover:underline-offset-8 decoration-purple-400 text-gray-800 text-lg' href="/">Home</Link></div> 
           <div className="flex justify-center gap-2 mx-4 items-center"><AiOutlineUser className="text-xl"/> <Link className='hover:underline hover:underline-offset-8 decoration-purple-400 text-gray-800 text-lg' href="/about">About</Link></div> 
