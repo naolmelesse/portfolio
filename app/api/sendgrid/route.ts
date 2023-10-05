83fe05abd9afc6f9838dafdfdcfa112eb2a2d3eb
Naol Melesse <naolmelesse91@gmail.com>
2023-10-05 13:20:39 +0300

    Sendgrid integrated

---
+++ app/api/sendgrid/route.ts
@@ -0,0 +1,114 @@
+import sendgrid from "@sendgrid/mail";
+import { NextResponse } from "next/server";
+
+const API_KEY : any = process.env.SENDGRID_API_KEY;
+sendgrid.setApiKey(API_KEY);
+
+export async function POST(req: any) {
+  try {
+    const data = await  req.json();
+    
+    await sendgrid.send({
+      to: "ndmelesse@gmail.com", // Your email where you'll receive emails
+      from: "naolmelesse91@gmail.com", // your website email address here
+      subject: `😁 ${data.subject} - ${data.fullname}`,
+      text: data.message,
+      html: `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Contact Form Submission</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  background-color: #F1EFEF;
+                  margin: 0;
+                  padding: 0;
+              }
+
+              .container {
+                  max-width: 600px;
+                  margin: 0 auto;
+                  padding: 20px;
+                  background-color: #ffffff;
+                  border-radius: 10px;
+                  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+              }
+
+              .header {
+                  text-align: center;
+                  background-color: #001524;
+                  padding: 20px;
+                  border-radius: 10px 10px 0 0;
+              }
+
+              .header h1 {
+                  font-size: 24px;
+                  color: #fff;
+              }
+
+              .content {
+                  padding: 20px;
+              }
+
+              .content p {
+                  font-size: 16px;
+                  line-height: 1.5;
+                  color: #333333;
+              }
+
+              .footer {
+                  text-align: center;
+                  background-color: #001524;
+                  padding: 10px;
+                  border-radius: 0 0 10px 10px;
+              }
+
+              .footer p {
+                  font-size: 14px;
+                  color: #fff;
+              }
+
+              .button {
+                  display: inline-block;
+                  padding: 10px 20px;
+                  background-color: #D6CC99;
+                  color: #fff;
+                  text-decoration: none;
+                  border-radius: 5px;
+                  margin-top: 20px;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <div class="header">
+                  <h1>Contact Form Submission</h1>
+              </div>
+              <div class="content">
+                  <p>Hello,</p>
+                  <p>You've received a new message through your portfolio website's contact form:</p>
+                  <p><strong>Name:</strong> ${data.fullname}</p>
+                  <p><strong>Email:</strong> ${data.email}</p>
+                  <p><strong>Message:</strong></p>
+                  <p>${data.message}</p>
+                  <a href="mailto:${data.email}" class="button">Reply to Message</a>
+              </div>
+              <div class="footer">
+                  <p>Thank you for using the contact form on https://naolmelesse.vercel.app .</p>
+              </div>
+          </div>
+      </body>
+      </html>
+`
+    });
+    console.log("hereeeee");
+    return NextResponse.json({ error: null });
+  } catch (error: any) {
+    console.log(error.message);
+    return NextResponse.json({ error: "Error occurred man: " + error.message });
+  }
+
+}
--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState } from "react";
 
 export default function Contact(){
@@ -5,20 +6,74 @@ export default function Contact(){
     const [email, setEmail] = useState("");
     const [subject, setSubject] = useState("");
     const [message, setMessage] = useState("");
+    const [errors, setErrors] = useState({});
+
+        //   Setting button text on form submission
+    const [buttonText, setButtonText] = useState("🚀 SEND MESSAGE");
+
+    // Setting success or failure messages states
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showFailureMessage, setShowFailureMessage] = useState(false);
     
-    //   const handleChange = (e: any) => {
-    //       console.log(e.target);
-    //     // const { name : string, value } = e.target;
-    //     // setFormData({
-    //     //   ...formData,
-    //     //   [name]: value,
-    //     // });
-    //   };
+    const handleValidation = () => {
+        let tempErrors :any = {};
+        let isValid = true;
     
-    //   const handleSubmit = (e: any) => {
-    //     e.preventDefault();
-    //     // Add code to handle form submission here (e.g., sending an email or API request).
-    //   };
+        if (fullname.length <= 0) {
+          tempErrors["fullname"] = true;
+          isValid = false;
+        }
+        if (email.length <= 0) {
+          tempErrors["email"] = true;
+          isValid = false;
+        }
+        if (subject.length <= 0) {
+          tempErrors["subject"] = true;
+          isValid = false;
+        }
+        if (message.length <= 0) {
+          tempErrors["message"] = true;
+          isValid = false;
+        }
+    
+        setErrors({ ...tempErrors });
+        // console.log("errors", errors);
+        return isValid;
+      };
+    
+      const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        let isValidForm = handleValidation();
+        
+        
+          const res = await fetch("/api/sendgrid", {
+            body: JSON.stringify({
+              email: email,
+              fullname: fullname,
+              subject: subject,
+              message: message,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+            method: "POST",
+          });
+
+          const { error } = await res.json();
+          console.log(error)
+          if (error) {
+            console.log(error);
+            setShowSuccessMessage(false);
+            setShowFailureMessage(true);
+            setButtonText("Message send failed ❌");
+            return;
+          }
+          setShowSuccessMessage(true);
+          setShowFailureMessage(false);
+          setButtonText("Message sent succesfully ✅");
+        
+    };
 
     return(
     <div className="flex flex-col lg:flex-row items-center justify-center w-full bg-cover text-gray-100 mx-[auto]"    style={{
@@ -26,7 +81,7 @@ export default function Contact(){
         }}>
             <h2 className="md:text-[4rem] text-center font-semibold mb-4">Make Contact</h2>
         <div className="lg:w-1/2 p-6">
-            <form method="POST">
+            <form onSubmit={handleSubmit}>
                 <div className="mb-4">
                     <label htmlFor="name" className="block text-gray-100 font-medium mb-2">Name<span className="text-red-500">*</span></label>
                     <input
@@ -84,7 +139,7 @@ export default function Contact(){
                     type="submit"
                     className="bg-transparent backdrop-blur border text-white py-2 px-4 rounded hover:backdrop-blur-lg transition duration-300"
                     >
-                    🚀 SEND MESSAGE
+                    {buttonText}
                     </button>
                 </div>
             </form>
