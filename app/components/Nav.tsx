0b718b01fc4800d114ae3716a0d00910d4e29b24
Naol Melesse <naolmelesse91@gmail.com>
2024-08-22 20:53:52 +0300

    fix project card issue, add experience, fix nav issue

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,30 +1,63 @@
-import Link from "next/link";
-import { BiHomeAlt2 } from "react-icons/bi";
-import { BsCodeSlash } from "react-icons/bs";
-import { FiMail } from "react-icons/fi";
-import { FaUserAstronaut } from "react-icons/fa";
-import { TbDownload } from "react-icons/tb"
-import Image from "next/image";
+import Link from 'next/link';
+import { BiHomeAlt2 } from 'react-icons/bi';
+import { BsCodeSlash } from 'react-icons/bs';
+import { FiMail } from 'react-icons/fi';
+import { FaUserAstronaut } from 'react-icons/fa';
+import { TbDownload } from 'react-icons/tb';
+import Image from 'next/image';
 
-export default function Nav(){
-    return(
-        <nav className='py-10 flex items-center justify-between lg:w-4/5 mx-[auto] bg-white px-10 md:px-20 lg:px-30'>
-        <Image
-          src="/logo.png"
-          alt="Naol Melesse Logo"
-          width={100}
-          height={38}
-        />
-        <div className='invisible lg:visible flex items-center p-2  bg-[#F5F5F5] z-10 fixed translate-x-[90%] rounded-full' >
-          <div className="flex justify-center gap-2 mx-4 items-center"><BiHomeAlt2 className="text-xl"/> <Link className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg' href="/">Home</Link></div> 
-          <div className="flex justify-center gap-2 mx-4 items-center"><FaUserAstronaut className="text-xl"/> <Link className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg' href="/about">About</Link></div> 
-          <div className="flex justify-center gap-2 mx-4 items-center"><BsCodeSlash className="text-xl"/> <Link className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg' href="#projects">Projects</Link></div> 
-          <div className="flex justify-center gap-2 mx-4 items-center"><FiMail className="text-xl"/> <Link className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg' href="#contact">Contact</Link></div> 
+export default function Nav() {
+  return (
+    <nav className='py-10 flex items-center justify-between lg:w-4/5 mx-[auto] bg-white px-10 md:px-20 lg:px-30'>
+      <Image src='/logo.png' alt='Naol Melesse Logo' width={100} height={38} />
+      <div className='invisible lg:visible flex items-center p-2  bg-[#F5F5F5] z-10 fixed translate-x-[50%] rounded-full'>
+        <div className='flex justify-center gap-2 mx-4 items-center'>
+          <BiHomeAlt2 className='text-xl' />{' '}
+          <Link
+            className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg'
+            href='/'
+          >
+            Home
+          </Link>
         </div>
+        <div className='flex justify-center gap-2 mx-4 items-center'>
+          <FaUserAstronaut className='text-xl' />{' '}
+          <Link
+            className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg'
+            href='/about'
+          >
+            About
+          </Link>
+        </div>
+        <div className='flex justify-center gap-2 mx-4 items-center'>
+          <BsCodeSlash className='text-xl' />{' '}
+          <Link
+            className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg'
+            href='#projects'
+          >
+            Projects
+          </Link>
+        </div>
+        <div className='flex justify-center gap-2 mx-4 items-center'>
+          <FiMail className='text-xl' />{' '}
+          <Link
+            className='hover:underline hover:underline-offset-8 decoration-[#F0DE36] text-gray-800 text-lg'
+            href='#contact'
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
 
-        <Link target="_blank" className='flex gap-1 items-center text-[#fff] bg-[#222831] transition duration-300 ease-in-out hover:bg-[#454545] px-4 py-2 rounded-lg' href="/Naol_Melesse-Resume(Modern).pdf" download="Naol_Melesse-Resume(Modern).pdf">Resume
-          <TbDownload className="text-lg"/>
-        </Link>
-      </nav>
-    )
+      <Link
+        target='_blank'
+        className='flex gap-1 items-center text-[#fff] bg-[#222831] transition duration-300 ease-in-out hover:bg-[#454545] px-4 py-2 rounded-lg'
+        href='/Naol_resume_2024.pdf'
+        download='Naol_resume_2024.pdf'
+      >
+        Resume
+        <TbDownload className='text-lg' />
+      </Link>
+    </nav>
+  );
 }
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,141 +1,128 @@
-import Image from "next/image";
-import furgo from "/public/furgo.png";
-import gobus from "/public/gobus.png";
-import dash from "/public/sms-dashboard.png";
-import mosaic from "/public/mosaic.png";
-import gameville from "/public/gameville.png";
-import { GoLinkExternal } from "react-icons/go";
-import Link from "next/link";
-import { Badge } from "@/components/ui/badge";
-import { Separator } from "@/components/ui/separator";
-
+import furgo from '/public/furgo.png';
+import gobus from '/public/gobus.png';
+import dash from '/public/sms-dashboard.png';
+import mosaic from '/public/mosaic.png';
+import gameville from '/public/gameville.png';
+import ProjectCard from './projectCard';
 
 export default function Projects() {
   return (
-    <div className="py-10]" id="projects">
+    <div className='py-10' id='projects'>
       <div>
-        <h3 className="text-3xl py-1  ">Projects</h3>
+        <h3 className='text-3xl py-1'>Projects</h3>
       </div>
-      <div className="flex flex-col items-center justify-center flex-wrap gap-10 py-10">
+      <div className='flex flex-col items-center justify-center flex-wrap gap-10 py-10'>
+        {/* School Management Dashboard */}
+        <ProjectCard
+          title={'Upcoming Project - A school management dashboard'}
+          details='An admin dashboard for a school with rich features.'
+          image={dash}
+          imageAltText='School management system dashboard'
+          technologies={[
+            'React',
+            'NextJS 14',
+            'TailwindCSS',
+            'React-chartjs-2',
+            'Auth0',
+          ]}
+        />
 
-        <div className="aspect-video px-5 py-2 md:my-10 border rounded-xl">
-          <h3 className="font-medium text-xl my-4">📌 Upcoming Project - A school management dashboard</h3>
-          <Image
-            alt={"School management system dashboard"}
-            src={dash}
-          />
-          <Separator className="my-4" />
-          <p className="font-medium my-2">Details:</p>
-          <p> An admin dashboard for a school with rich features.</p>
+        {/* GoBus - A bus booking web-app */}
+        <ProjectCard
+          title={'GoBus - A bus booking web-app'}
+          details='A bus ticket booking web app for easy ticket booking and much more.'
+          image={gobus}
+          imageAltText='Bus ticket booking website landing page'
+          technologies={[
+            'NextJS',
+            'TailwindCSS',
+            'Strapi',
+            'PostgreSQL',
+            'Cloudinary',
+            'Stripe',
+            'Auth0',
+          ]}
+          hasLinks={true}
+          hasMoreText={true}
+          links={{
+            githubLink: 'https://github.com/naolmelesse/gobus',
+            liveLink: 'https://gobus-liard.vercel.app/',
+          }}
+          moreText={[
+            {
+              name: 'Tip!',
+              content: ' I hear there is a bus from Addis Ababa to Dire Dawa.',
+            },
+            {
+              name: 'Payment:',
+              content: ' Use card: 4242 4242 4242 4242 EXP:04/24 CVC:424',
+            },
+          ]}
+        />
 
-          <div className="flex gap-3 flex-wrap my-5">
-            <Badge variant="outline">React</Badge>
-            <Badge variant="outline">NextJS 14</Badge>
-            <Badge variant="outline">TailwindCSS</Badge>
-            <Badge variant="outline">React-chartjs-2</Badge>
-            <Badge variant="outline">Auth0</Badge>
-          </div>
+        {/* MOSAIC - An online art marketplace */}
+        <ProjectCard
+          title={'MOSAIC - An online art marketplace'}
+          details='A web app to explore fine arts and purchase.'
+          image={mosaic}
+          imageAltText='An online fine arts marketplace'
+          technologies={[
+            'NextJS',
+            'TailwindCSS',
+            'Auth0',
+            'Stripe',
+            'PostgreSQL',
+            'Strapi',
+            'Cloudinary',
+          ]}
+          hasLinks={true}
+          hasMoreText={true}
+          links={{
+            githubLink: 'https://github.com/naolmelesse/mosaic',
+            liveLink: 'https://mosaic-silk.vercel.app/',
+          }}
+          moreText={[
+            { name: 'Tip!', content: " Use coupon - 'FIRSTBUY'" },
+            {
+              name: 'Payment:',
+              content: ' Use card: 4242 4242 4242 4242 EXP:04/24 CVC:424',
+            },
+          ]}
+        />
 
-        </div>
+        {/* GameVille - Discover and search games */}
+        <ProjectCard
+          title={'GameVille - Discover and search games'}
+          details='A web app to discover and search games.'
+          image={gameville}
+          imageAltText='Game discover website'
+          technologies={[
+            'NextJS',
+            'TailwindCSS',
+            'REST API',
+            'RAWG API',
+            'Shadcn UI',
+          ]}
+          hasLinks={true}
+          links={{
+            githubLink: 'https://github.com/naolmelesse/game-ville',
+            liveLink: 'https://game-ville.vercel.app/',
+          }}
+        />
 
-        <div className="aspect-video px-5 py-2 md:my-10 border rounded-xl">
-          <h3 className="font-medium text-xl my-4">📌 GoBus - A bus booking web-app</h3>
-          <Image
-            alt={"Bus ticket booking website landing page"}
-            src={gobus}
-          />
-          <Separator className="my-4" />
-          <p className="font-medium my-2">Details:</p>
-          <p> A bus ticket booking web app for easy ticket booking and much more.</p>
-          <p className="my-2"><span className="font-medium">Tip!</span> I hear there is a bus from Addis Ababa to Dire Dawa.</p>
-          <p className="my-2"><span className="font-medium">Payment</span> Use card: 4242 4242 4242 4242 EXP:04/24 CVC:424</p>
-
-          <div className="flex gap-3 flex-wrap my-5">
-            <Badge variant="outline">NextJS</Badge>
-            <Badge variant="outline">TailwindCSS</Badge>
-            <Badge variant="outline">Strapi</Badge>
-            <Badge variant="outline">PostgreSQL</Badge>
-            <Badge variant="outline">Cloudinary</Badge>
-            <Badge variant="outline">Stripe</Badge>
-            <Badge variant="outline">Auth0</Badge>
-          </div>
-          <div className="flex gap-5 my-2">
-            <Link className=" bg-[#000] text-[#fff] py-2 px-4 rounded-lg flex items-center gap-2 hover:bg-[#444] transition duration-300" href="https://github.com/naolmelesse/gobus" target="_blank">View on Github<GoLinkExternal className="inline text-[1.5rem]" /></Link>
-            <Link className=" bg-[#000] text-[#fff] py-2 px-4 rounded-lg flex items-center gap-2 hover:bg-[#444] transition duration-300" href="https://gobus-liard.vercel.app/" target="_blank">View Live <GoLinkExternal className="inline text-[1.5rem]" /></Link>
-          </div>
-        </div>
-
-        <div className="aspect-video px-5 py-2 md:my-10 border rounded-xl">
-          <h3 className="font-medium text-xl my-4">📌 MOSAIC - An online art marketplace</h3>
-          <Image
-            alt={"An online fine arts marketplace"}
-            src={mosaic}
-          />
-          <Separator className="my-4" />
-          <p className="font-medium my-2">Details:</p>
-          <p>A web app to explore fine arts and purchase </p>
-          <p className="my-2"><span className="font-medium">Tip!</span> Use coupon - &apos;FIRSTBUY&apos;</p>
-          <p className="my-2"><span className="font-medium">Payment</span> Use card: 4242 4242 4242 4242 EXP:04/24 CVC:424</p>
-          <div className="flex gap-3 flex-wrap my-5">
-            <Badge variant="outline">NextJS</Badge>
-            <Badge variant="outline">TailwindCSS</Badge>
-            <Badge variant="outline">Auth0</Badge>
-            <Badge variant="outline">Stripe</Badge>
-            <Badge variant="outline">PostgreSQL</Badge>
-            <Badge variant="outline">Strapi</Badge>
-            <Badge variant="outline">Cloudinary</Badge>
-          </div>
-          <div className="flex gap-5 my-2">
-            <Link className=" bg-[#000] text-[#fff] py-2 px-4 rounded-lg flex items-center gap-2 hover:bg-[#444] transition duration-300" href="https://github.com/naolmelesse/mosaic" target="_blank">View on Github <GoLinkExternal className="inline text-[1.5rem]" /></Link>
-            <Link className=" bg-[#000] text-[#fff] py-2 px-4 rounded-lg flex items-center gap-2 hover:bg-[#444] transition duration-300" href="https://mosaic-silk.vercel.app/" target="_blank">View Live <GoLinkExternal className="inline text-[1.5rem]" /></Link>
-          </div>
-
-        </div>
-
-        <div className="aspect-video px-5 py-2 md:my-10 border rounded-xl">
-          <h3 className="font-medium text-xl my-4">📌 GameVille - Discover and search games</h3>
-          <Image
-            alt={"Game disover website"}
-            src={gameville}
-          />
-          <Separator className="my-4" />
-          <p className="font-medium my-2">Details:</p>
-          <p>A web app to discover and search games</p>
-          <div className="flex gap-3 flex-wrap my-5">
-            <Badge variant="outline">NextJS</Badge>
-            <Badge variant="outline">TailwindCSS</Badge>
-            <Badge variant="outline">REST API</Badge>
-            <Badge variant="outline">RAWG API</Badge>
-            <Badge variant="outline">Shadcn UI</Badge>
-          </div>
-          <div className="flex gap-5 my-2">
-            <Link className=" bg-[#000] text-[#fff] py-2 px-4 rounded-lg flex items-center gap-2 hover:bg-[#444] transition duration-300" href="https://github.com/naolmelesse/game-ville" target="_blank">View on Github <GoLinkExternal className="inline text-[1.5rem]" /></Link>
-            <Link className=" bg-[#000] text-[#fff] py-2 px-4 rounded-lg flex items-center gap-2 hover:bg-[#444] transition duration-300" href="https://game-ville.vercel.app/" target="_blank">View Live <GoLinkExternal className="inline text-[1.5rem]" /></Link>
-          </div>
-        </div>
-
-        <div className="aspect-video px-5 py-2 md:my-10 border rounded-xl">
-          <h3 className="font-medium text-xl my-4">📌 Furgo - A car sharing web-app</h3>
-          <Image
-            alt={"car sharing Web landing page"}
-            src={furgo}
-          />
-          <Separator className="my-4" />
-          <p className="font-medium my-2">Details:</p>
-          <p>Airbnb but for cars. Two type of users: Hosts and renters. </p>
-          <div className="flex gap-3 flex-wrap my-5">
-            <Badge variant="outline">ReactJs</Badge>
-            <Badge variant="outline">NodeJs</Badge>
-            <Badge variant="outline">ExpressJs</Badge>
-            <Badge variant="outline">MongoDB</Badge>
-            <Badge variant="outline">Stripe</Badge>
-          </div>
-          <div className="my-2 flex">
-            <Link className=" bg-[#000] text-[#fff] py-2 px-4 rounded-lg flex items-center gap-2 hover:bg-[#444] transition duration-300" href="https://github.com/naolmelesse/furgo" target="_blank">View on Github <GoLinkExternal className="inline text-[1.5rem]" /></Link>
-          </div>
-        </div>
-
+        {/* Furgo - A car sharing web-app */}
+        <ProjectCard
+          title={'Furgo - A car sharing web-app'}
+          details='Airbnb but for cars. Two types of users: Hosts and renters.'
+          image={furgo}
+          imageAltText='Car sharing web landing page'
+          technologies={['ReactJs', 'NodeJs', 'ExpressJs', 'MongoDB', 'Stripe']}
+          hasLinks={true}
+          links={{
+            githubLink: 'https://github.com/naolmelesse/furgo',
+          }}
+        />
       </div>
     </div>
-  )
+  );
 }
